e9bd56679e345bbdd78cd0e5e0d0f06055eb7bf5
Joshua Esrig <joshuaesrig@Joshuas-MacBook-Air.local>
2025-01-12 12:05:29 -0600

    uid and token verification added and functional

--- src/main/java/com/example/l2g/controller/OrderController.java
+++ src/main/java/com/example/l2g/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.example.l2g.controller;
 
 import com.example.l2g.model.receiving.CustomerOrder;
 import com.example.l2g.model.sending.StockedProduct;
+import com.example.l2g.service.FirebaseService;
 import com.example.l2g.service.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,25 +19,31 @@ import java.util.Map;
 public class OrderController {
 
     private final OrderService orderService;
+    private final FirebaseService firebaseService;
 
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, FirebaseService firebaseService) {
         this.orderService = orderService;
+        this.firebaseService = firebaseService;
     }
 
 
     // Handle POST request to create a new order
     @PostMapping("/create")
-    public ResponseEntity<CustomerOrder> createOrder(@Valid @RequestBody CustomerOrder order) {
+    public ResponseEntity<CustomerOrder> createOrder(
+            @RequestHeader("uid") String uid,
+            @RequestHeader("token") String token,
+            @Valid @RequestBody CustomerOrder order) {
+
         //add auth verification
+        if (!firebaseService.validateUser(token, uid)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+
         orderService.createCustomerOrder(order);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
-//
-//    @PostMapping("/batch")
-//    public List<StockedProduct> getBatchProducts(@RequestBody CustomerOrder order) {
-//        // Fetch batch products from the service
-//        return productService.
-//    }
+
 
 }
